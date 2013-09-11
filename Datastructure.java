f53e6ed9a20792001df529c0e74cb06418442c0a
Schalk Jacobus Jordaan <schalkjordaan@gmail.com>
2013-09-11 22:47:23 +0200

    .

---
+++ Datastructure.java
@@ -0,0 +1,8 @@
+//the abstract class from which all other classes 
+public abstract class Datastructure 
+{
+	public abstract String search(String name, String field);
+	public abstract String searchAll(String name, String field);
+	public abstract String findRandom();
+	public abstract void accessAll();
+}
